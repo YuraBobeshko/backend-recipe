bdaf61f1f5207cc42ec6a5756e32312d0fb9c73d
YuraBobeshko <gorobech55@gmail.com>
2021-02-23 19:59:31 +0200

    [update] script db clear

---
+++ src/scripts/clearDB.js
@@ -0,0 +1,8 @@
+import { sequelize } from "../models";
+
+(async () => {
+  await sequelize.drop({ cascade: true });
+  // await sequelize.query("TRUNCATE `sequelizedata`;");
+  await sequelize.query("DELETE FROM `sequelizemeta`;");
+  process.exit(0);
+})();
