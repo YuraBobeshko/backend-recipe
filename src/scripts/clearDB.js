3f792b539163ae80aea09d186c6c7e7e0a5f0f5a
YuraBobeshko <gorobech55@gmail.com>
2021-02-28 10:04:21 +0200

    [fix] bug in script

--- src/scripts/clearDB.js
+++ src/scripts/clearDB.js
@@ -2,7 +2,7 @@ import { sequelize } from "../models";
 
 (async () => {
   await sequelize.drop({ cascade: true });
-  // await sequelize.query("TRUNCATE `sequelizedata`;");
+  await sequelize.query("TRUNCATE `sequelizedata`;");
   await sequelize.query("DELETE FROM `sequelizemeta`;");
   process.exit(0);
 })();
